979a82554991d76334b4e6fc4e0beba4f5b5d3db
Antti Hamara <antti.hamara@sulake.com>
2021-01-31 01:04:04 +0200

    Mangetic Glow

--- Assets/Scripts/Magnetic.cs
+++ Assets/Scripts/Magnetic.cs
@@ -10,8 +10,37 @@ public class Magnetic : MonoBehaviour
     public bool IsMagnetic;
     public int TreasureID = -1;
 
+    [ColorUsage(true, true)]
+    public Color color;
+    [ColorUsage(true, true)]
+    public Color color2;
+    public Renderer objectRenderer;
+
+    private GameObject Magnet;
+
     private void Awake()
     {
         IsMagnetic = true;
+        Magnet = GameObject.Find("Magnet");
+        //objectRenderer.sharedMaterial.SetColor("_Color", color);
+        objectRenderer.material.SetColor("_EmissionColor", color);
+        objectRenderer.material.SetColor("_BaseColor", color);
+
+    }
+
+    private void Update()
+    {
+        float distance = Vector3.Distance(gameObject.transform.position, Magnet.transform.position);
+
+        if (distance < 10)
+        {
+            Debug.Log(objectRenderer.material.name);
+            float t = Mathf.InverseLerp(10, 2, distance);
+            Color color3 = Color.Lerp(color, color2, t);
+
+
+            objectRenderer.material.SetColor("_EmissionColor", color3);
+        }
+
     }
 }
