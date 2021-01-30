44d1563b035e237fea5860457c870fa375fefa24
Olli Etuaho <olli.etuaho@gmail.com>
2021-01-30 10:49:15 +0200

    Add some tilting to the boat

--- Assets/Scripts/BoatController.cs
+++ Assets/Scripts/BoatController.cs
@@ -13,18 +13,32 @@ public class BoatController : MonoBehaviour
     [SerializeField] private float HorizontalAcceleration = 1.0f;
     [SerializeField] private float HorizontalMaxSpeed = 5.0f;
 
+    [SerializeField] private float TiltAcceleration = 1.0f;
+    [SerializeField] private float TiltSpringConstant = 1.0f;
+
+    private float AngularVelocity;
+    private float TiltAngle;
+
     private float HorizontalSpeed;
 
     private void Update ()
     {
         HorizontalInput = Input.GetAxis("Horizontal");
+        AngularVelocity = 0.0f;
+        TiltAngle = 0.0f;
     }
 
     private void FixedUpdate()
     {
+        AngularVelocity += (HorizontalInput + Mathf.Sin(Time.fixedTime) * 0.3f) * TiltAcceleration * Time.fixedDeltaTime;
+        AngularVelocity -= TiltAngle * TiltSpringConstant * Time.fixedDeltaTime;
+        AngularVelocity = AngularVelocity * 0.99f;
+        TiltAngle += AngularVelocity * Time.fixedDeltaTime;
+
         HorizontalSpeed += HorizontalInput * HorizontalAcceleration * Time.fixedDeltaTime;
         HorizontalSpeed *= 0.99f;
         HorizontalSpeed = Mathf.Clamp(HorizontalSpeed, -HorizontalMaxSpeed, HorizontalMaxSpeed);
         RB.MovePosition(RB.position + HorizontalSpeed * Vector3.right * Time.fixedDeltaTime);
+        RB.MoveRotation(Quaternion.AngleAxis(TiltAngle, Vector3.forward));
     }
 }
