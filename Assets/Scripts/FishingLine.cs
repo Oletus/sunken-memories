34b52221dd53c0d823aa6982886790297a4a1a55
Olli Etuaho <olli.etuaho@gmail.com>
2021-01-30 09:38:39 +0200

    Fishing line uses joint instead of a kinematic body

--- Assets/Scripts/FishingLine.cs
+++ Assets/Scripts/FishingLine.cs
@@ -9,6 +9,8 @@ public class FishingLine : MonoBehaviour
 {
     [SerializeField] private Rigidbody2D RopeSectionPrefab;
 
+    [SerializeField] private HingeJoint2D RopeTopJoint;
+
     [SerializeField] private float RopeSectionLength = 1.0f;
 
     [SerializeField] private int MaxSectionCount = 20;
@@ -19,6 +21,8 @@ public class FishingLine : MonoBehaviour
 
     private float FishingLineSpeedDown;
 
+    private float TopJointAnchor; // A value of -0.5 means that the section is fully retracted, a value of 0.5 means fully extended.
+
     private void Awake ()
     {
         RopeSections = new List<Rigidbody2D>();
@@ -28,12 +32,10 @@ public class FishingLine : MonoBehaviour
     private void AddRopeTopSection()
     {
         Rigidbody2D newTopSection = Instantiate(RopeSectionPrefab, transform.position, transform.rotation);
-        newTopSection.isKinematic = true;
 
-        if ( RopeSections.Count > 0 )
+        if (RopeSections.Count > 0)
         {
             Rigidbody2D prevTopSection = RopeSections[RopeSections.Count - 1];
-            prevTopSection.isKinematic = false;
             newTopSection.position = prevTopSection.position + Vector2.up * RopeSectionLength;
             HingeJoint2D joint = newTopSection.gameObject.AddComponent<HingeJoint2D>();
             joint.connectedBody = prevTopSection;
@@ -41,7 +43,15 @@ public class FishingLine : MonoBehaviour
             joint.anchor = Vector2.down * RopeSectionLength * 0.5f;
             joint.connectedAnchor = Vector2.up * RopeSectionLength * 0.5f;
             joint.enabled = true;
+            TopJointAnchor -= 1.0f;
         }
+        else
+        {
+            TopJointAnchor = -RopeSectionLength * 0.4999f;
+        }
+        RopeTopJoint.connectedBody = newTopSection;
+        RopeTopJoint.connectedAnchor = Vector2.up * TopJointAnchor;
+        RopeTopJoint.enabled = true;
 
         RopeSections.Add(newTopSection);
     }
@@ -55,26 +65,34 @@ public class FishingLine : MonoBehaviour
         Rigidbody2D last = RopeSections[RopeSections.Count - 1];
         Destroy(last.gameObject);
         RopeSections.RemoveAt(RopeSections.Count - 1);
-        if ( RopeSections.Count > 0 )
+
+        TopJointAnchor += 1.0f;
+
+        if (RopeSections.Count > 0)
         {
-            RopeSections[RopeSections.Count - 1].isKinematic = true;
+            RopeTopJoint.connectedBody = RopeSections[RopeSections.Count - 1];
+            RopeTopJoint.connectedAnchor = Vector2.up * TopJointAnchor;
+        }
+        else
+        {
+            RopeTopJoint.enabled = false;
         }
     }
 
     private void Update ()
     {
-        FishingLineVerticalButton = Input.GetButton("FishingLineVertical");
+        FishingLineVerticalButton = Input.GetButton("Vertical");
     }
 
     private void FixedUpdate()
     {
         if ( FishingLineVerticalButton )
         {
-            FishingLineSpeedDown = Mathf.MoveTowards(FishingLineSpeedDown, -0.1f, Time.fixedDeltaTime);
+            FishingLineSpeedDown = Mathf.MoveTowards(FishingLineSpeedDown, -5.0f, 50.0f * Time.fixedDeltaTime);
         }
         else
         {
-            FishingLineSpeedDown = Mathf.MoveTowards(FishingLineSpeedDown, 0.1f, Time.fixedDeltaTime);
+            FishingLineSpeedDown = Mathf.MoveTowards(FishingLineSpeedDown, 5.0f, 50.0f * Time.fixedDeltaTime);
         }
         if ( RopeSections.Count == 0 )
         {
@@ -84,23 +102,24 @@ public class FishingLine : MonoBehaviour
         {
             Rigidbody2D lastSection = RopeSections[RopeSections.Count - 1];
             float useSpeedDown = FishingLineSpeedDown;
-            if ( RopeSections.Count >= MaxSectionCount )
-            {
-                useSpeedDown = Mathf.Min(FishingLineSpeedDown, 0.0f);
-            }
+            TopJointAnchor += useSpeedDown * Time.fixedDeltaTime;
             if ( RopeSections.Count == 1 )
             {
-                useSpeedDown = Mathf.Max(FishingLineSpeedDown, 0.0f);
+                TopJointAnchor = Mathf.Max(TopJointAnchor, -0.5f);
             }
-            lastSection.MovePosition(lastSection.position + Vector2.down * useSpeedDown);
-            if (lastSection.position.y < transform.position.y - RopeSectionLength * 0.5f)
+            if ( RopeSections.Count >= MaxSectionCount )
+            {
+                TopJointAnchor = Mathf.Min(TopJointAnchor, 0.5f);
+            }
+            if (TopJointAnchor > RopeSectionLength * 0.51f)
             {
                 AddRopeTopSection();
             }
-            if ( lastSection.position.y > transform.position.y + RopeSectionLength * 0.6f )
+            else if (TopJointAnchor < -RopeSectionLength * 0.51f)
             {
                 RemoveRopeTopSection();
             }
+            RopeTopJoint.connectedAnchor = Vector2.up * TopJointAnchor;
         }
     }
 }
